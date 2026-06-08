c8ebb176ba9fe7445f98d7f14b4c10c065e06cfc
jatoapan <jatoapan@espol.edu.ec>
2026-06-08 01:33:53 -0500

    fix: add HasApiTokens to User model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -9,10 +9,11 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasUuids, SoftDeletes, HasRoles;
+    use HasApiTokens, HasFactory, Notifiable, HasUuids, SoftDeletes, HasRoles;
 
     protected $fillable = [
         'name',
